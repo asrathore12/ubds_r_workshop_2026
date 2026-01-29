29ea49d98dbc711d97b91b981daceea270227aa0
Abhishek Singh Rathore <abhishek_mac@Abhisheks-MacBook-Pro-2.local>
2026-01-29 15:29:35 -0600

    reorg

---
+++ analysis/src/my_r_script.R
@@ -0,0 +1,37 @@
+# this is a comment
+
+100 + 101
+
+3 + 5 * 10
+
+#remind me the order of operation
+3 + (5*10)
+
+(3+5)*10
+#exponent?
+3^10
+
+
+# are these equal
+
+10 == 10
+9==10
+
+
+#computer are not great with desimal
+10.01==10.01
+
+10.01==10.0000000001
+
+10.00==10.00000000000000000000000000000000000000000000001
+
+
+
+#R has functions
+getwd()
+sin(1)
+
+x <- 1/40 
+  
+  
+  
